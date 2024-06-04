088d13956379b08ebcd6ab6b2e68636f38bd60c7
Marcelle Vargas <marcellecode@gmail.com>
2024-06-04 18:59:43 -0300

    [feat]- paws webpage

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { createApp } from "vue";
+import { createWebHistory, createRouter } from "vue-router";
+
+import Home from "./views/Home.vue";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import App from "@/App.vue";
+
+const routes = [
+  { path: "/home", component: Home },
+  { path: "/:pathMatch(.*)*", redirect: "/home" }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App).use(router).mount("#app");
